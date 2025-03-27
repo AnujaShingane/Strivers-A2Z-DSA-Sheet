5d802cdac550626c77c6e50f9d94b64c21bf716f
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-03-27 16:38:29 +0530

    Update Segrregate odd and even nodes in LL.java

--- LinkedList/Medium/Segrregate odd and even nodes in LL.java
+++ LinkedList/Medium/Segrregate odd and even nodes in LL.java
@@ -10,40 +10,19 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode oddHead = null, oddTail = null, evenHead = null, evenTail = null;
-        int count = 1;
-        while(head != null){
-            // Even
-            if(count % 2 == 0){
-                if(evenHead == null){
-                    evenHead = head;
-                    evenTail = head;
-                }
-                else{
-                    evenTail.next = head;
-                    evenTail = evenTail.next;
-                }
-            }
-            // Odd
-            if(count % 2 != 0){
-                if(oddHead == null){
-                    oddHead = head;
-                    oddTail = head;
-                }
-                else{
-                    oddTail.next = head;
-                    oddTail = evenTail.next;
-                }
-            }
-            count++;
-            head = head.next;
-        }
-        if(evenHead == null){
-            return oddHead;
-        }
-        evenTail.next = null;
-        oddTail.next = evenHead;
+        if (head == null || head.next == null) return head;
+
+        ListNode odd = head, even = head.next, evenHead = even;
         
-        return oddHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = evenHead; // Connect odd and even lists
+        return head;
     }
 }
+
