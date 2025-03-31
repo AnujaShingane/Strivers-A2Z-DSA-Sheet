e5016f0382667fbdfd3e2426ae7982fcb62e0da4
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-01 00:01:09 +0530

    Update Left Rotate Array by K Places.java

--- Arrays/Easy/Left Rotate Array by K Places.java
+++ Arrays/Easy/Left Rotate Array by K Places.java
@@ -9,6 +9,29 @@
    array by 2 will make the output array .
 
  */
+My approach -->
+
+   class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] temp = new int[nums.length];
+        for(int i = 0 ; i < n ; i++){
+            temp[i] = nums[i];
+        }
+
+        k = k%n;
+        int j = 0;
+        for(int i = n-k ; i < n ; i++){
+            nums[j++] = temp[i];
+        }
+        
+        for(int i = 0 ; i < n-k ; i++){
+            nums[j++] = temp[i];
+        }
+    }
+}
+
+
 
 public class Left_Rotate_an_array_by_K_place {
     public static void rotate(int[] nums, int k) {
