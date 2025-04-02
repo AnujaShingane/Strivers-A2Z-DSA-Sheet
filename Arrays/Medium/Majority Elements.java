18ceca1b6239d8c9fa713929c93e1654d4232523
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-02 22:21:07 +0530

    Update Majority Elements.java

--- Arrays/Medium/Majority Elements.java
+++ Arrays/Medium/Majority Elements.java
@@ -21,6 +21,31 @@
 // import java.util.Arrays;
 // import java.util.HashMap;
 
+
+
+***
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+
+        int n = nums.length/2;
+        
+        for (int num : nums) {
+            hmap.put(num, hmap.getOrDefault(num, 0) + 1);
+        }
+
+        for(int key : hmap.keySet()){
+            if(hmap.get(key) > n){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+            
 public class MajorityElement {
     public static int majorityElement(int[] nums) {
         /*
