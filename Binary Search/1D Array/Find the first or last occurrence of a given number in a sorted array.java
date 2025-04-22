9d362669d0fd9914e1cc4f0cdde740fd81b63d54
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-22 17:15:25 +0530

    Update Find the first or last occurrence of a given number in a sorted array.java

--- Binary Search/1D Array/Find the first or last occurrence of a given number in a sorted array.java
+++ Binary Search/1D Array/Find the first or last occurrence of a given number in a sorted array.java
@@ -18,6 +18,63 @@ import java.util.Arrays;
    Output: -1, -1
    Explanation: Target value 60 is not present in the array 
  */
+optimised --
+class Solution {
+    public int firstOccurance(int[] nums, int target){
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                high = mid-1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    public int lastOccurance(int[] nums, int target){
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                low = mid+1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[2];
+        int first = firstOccurance(nums,target);
+        if(first == -1){
+            res[0] = -1;
+            res[1] = -1;
+        }
+        int last = lastOccurance(nums,target);
+        res[0] = first;
+        res[1] = last;
+        return res;
+    }
+}
 
 
 public class Find_the_first_or_last_occurrence_of_a_given_number_in_a_sorted_array {
