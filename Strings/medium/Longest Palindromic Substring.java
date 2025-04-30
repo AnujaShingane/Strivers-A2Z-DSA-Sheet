0477154c5bf21980097de4ab72b75285b2bec3ef
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-30 23:22:22 +0530

    Update Longest Palindromic Substring.java

--- Strings/medium/Longest Palindromic Substring.java
+++ Strings/medium/Longest Palindromic Substring.java
@@ -14,6 +14,35 @@
    Output: "bb"
 */
 
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String longest = "";
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; i < n ; j++){
+                String sub = s.substring(i,j+1);
+                if(isPalindrome(sub) && sub.length() > longest.length()){
+                    longest = sub;
+                }
+            }
+        }
+        return longest;
+    }
+
+    public boolean isPalindrome(String str){
+        int left = 0;
+        int right = str.length() - 1;
+
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
 
 public class Longest_Palindromic_Substring {
     public static boolean palindrome(String str){
