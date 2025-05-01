f1e41db8f4cb7f4441b980aabf0a636cda361c83
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-05-01 09:56:27 +0530

    Update Roman Number to Integer and vice versa.java

--- Strings/medium/Roman Number to Integer and vice versa.java
+++ Strings/medium/Roman Number to Integer and vice versa.java
@@ -37,7 +37,33 @@
    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 */
 
+brute
 
+class Solution {
+    public int romanToInt(String s) {
+        int n = s.length();
+        if(s.isEmpty())return 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int result = map.get(s.charAt(n-1));
+
+        for(int i = s.length()-2 ; i >= 0 ; i--){
+            if(map.get(s.charAt(i+1)) > map.get(s.charAt(i))){
+                result -= map.get(s.charAt(i));
+            }else if(map.get(s.charAt(i+1)) <= map.get(s.charAt(i))){
+                result += map.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+}
 
 import java.util.HashMap;
 
