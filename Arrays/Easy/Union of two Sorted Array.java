e0c2dfe1696abb4aeefca752751739885f9d8433
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-02 17:18:51 +0530

    Update Union of two Sorted Array.java

--- Arrays/Easy/Union of two Sorted Array.java
+++ Arrays/Easy/Union of two Sorted Array.java
@@ -34,6 +34,77 @@
 
  */
 
+
+
+
+
+
+
+
+
+
+
+// User function Template for Java
+
+class Solution {
+    public static int findUnion(int a[], int b[]){
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int n = a.length;
+        int m = b.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = 0 , j = 0;
+
+        while(i < n && j < m){
+            if(a[i] <= b[j]){
+                if(list.isEmpty() || list.get(list.size()-1) != a[i]){
+                    list.add(a[i]);
+                }
+                i++;
+            }else if(a[i] > b[j]){
+                if(list.isEmpty() || list.get(list.size()-1) != b[j]){
+                    list.add(b[j]);
+                }
+                j++;
+            }
+        }
+        
+        while(i < n){
+            if(list.isEmpty() || list.get(list.size()-1) != a[i]){
+                list.add(a[i]);
+            }
+            i++;
+        }
+        
+        while(j < m){
+            if(list.isEmpty() || list.get(list.size()-1) != b[j]){
+                list.add(b[j]);
+            }
+            j++;
+        }
+        
+        return list.size();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import java.util.*;
 
 public class Union_Of_Two_Sorted_Array {
