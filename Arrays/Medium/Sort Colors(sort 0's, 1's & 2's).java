36c1ab5d0cb47fbf46501d3d887f544c3e6a786d
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-05 17:30:11 +0530

    Update Sort Colors(sort 0's, 1's & 2's).java

--- Arrays/Medium/Sort Colors(sort 0's, 1's & 2's).java
+++ Arrays/Medium/Sort Colors(sort 0's, 1's & 2's).java
@@ -5,6 +5,51 @@
 
 
 
+
+
+1 --> Arrays.sort(arr);
+
+
+Initialize: low = 0, mid = 0, high = n - 1
+Loop while mid <= high:
+If nums[mid] == 0 --> swap with low, increment both low and mid
+If nums[mid] == 1  -->  just move mid forward
+If nums[mid] == 2  -->  swap with high, decrement high
+
+
+
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
+
+
+
+            
+
+
 /*
  * Sort an array of 0s, 1s and 2s
    Problem Statement: Given an array consisting of only 0s, 1s and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
