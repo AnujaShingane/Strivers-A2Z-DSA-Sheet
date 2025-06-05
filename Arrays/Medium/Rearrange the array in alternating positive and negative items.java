a3d248b5401ec107dade4b66c3dac1fbbb565d18
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-05 16:41:05 +0530

    Update Rearrange the array in alternating positive and negative items.java

--- Arrays/Medium/Rearrange the array in alternating positive and negative items.java
+++ Arrays/Medium/Rearrange the array in alternating positive and negative items.java
@@ -1,3 +1,90 @@
+Brute:-
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] >= 0){
+                pos.add(nums[i]);
+            }else{
+                neg.add(nums[i]);
+            }
+        }
+
+        for(int i = 0 ; i < n/2 ; i++){
+            res[2*i] = pos.get(i);
+            res[2*i+1] = neg.get(i);
+        }
+
+        return res;
+    }
+}
+
+
+Time Complexity:
+O(n) --> for the loop that separates positive and negative numbers.
+O(n/2)  -->  to fill the result array.
+Total: O(n)
+
+🧠 Space Complexity:
+O(n)  -->  for the result array
+O(n)  -->  for the pos list
+O(n)   --> for the neg list
+Total: O(3n) = O(n) extra space
+
+
+
+
+
+Optimal :-
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] dummy = new int[n];
+        int pos = 0;
+        int neg = 1;
+
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] >= 0){
+                dummy[pos] = nums[i];
+                pos = pos+2;
+            }else{
+                dummy[neg] = nums[i];
+                neg = neg+2;
+            }
+        }
+        return dummy;
+    }
+}
+
+
+
+Time Complexity:
+Single pass through the array → O(n)
+
+Space Complexity:
+Only one int[] result array of size n → O(n)
+No extra lists used
+
+
+
+
+
+
+🏁 Summary Table:
+Approach	       Time Complexity	Space Complexity	Extra Lists Used
+Brute Force        	O(n)	          O(3n) → O(n)	      Yes (2 lists)
+Optimal           	O(n)	           O(n)	               No
+
+
+
+
+
 import java.util.Arrays;
 
 public class Rearrange_Array_Elements_by_Sign_Positive_Negative {
