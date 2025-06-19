360b3768763afb2ef9eb854d6b3cd0f6a1fd6ddc
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-19 21:41:29 +0530

    Update Number of substring containing all three characters.java

--- Sliding Window/Medium Problems/Number of substring containing all three characters.java
+++ Sliding Window/Medium Problems/Number of substring containing all three characters.java
@@ -22,6 +22,39 @@
    Output: 1
  */
 
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int goal = 3;
+        return number(s, goal) - number(s, goal-1);
+    }
+
+    public int number(String s, int goal) {
+        int n = s.length();
+        int l = 0 , r = 0;
+        int cnt = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        if(goal < 0) return 0;
+
+        while(r < n){
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r),0)+1);
+
+            while(map.size() > goal){
+                map.put(s.charAt(l), map.get(s.charAt(l))-1);
+                if(map.get(s.charAt(l)) == 0){
+                    map.remove(s.charAt(l));
+                }
+                l++;
+            }
+
+            if(map.size() <= goal){
+                cnt +=  r-l+1;
+            }
+            r++;
+        }
+        return cnt;
+    }
+}
+
 
 public class Number_of_Substrings_Containing_All_Three_Characters {
     public static int numberOfSubstrings(String s) {
