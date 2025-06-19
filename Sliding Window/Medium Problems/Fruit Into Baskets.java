44240d8a2e2af6d9526526598541a16c9e8f38fd
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-19 21:36:57 +0530

    Update Fruit Into Baskets.java

--- Sliding Window/Medium Problems/Fruit Into Baskets.java
+++ Sliding Window/Medium Problems/Fruit Into Baskets.java
@@ -28,6 +28,37 @@
  */
 
 
+My Solution -->
+   
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        int l = 0 , r = 0;
+        int maxlen = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        while(r < n){
+            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
+
+            if(map.size() > 2){
+                map.put(fruits[l],map.get(fruits[l])-1);
+                if(map.get(fruits[l]) == 0){
+                    map.remove(fruits[l]);
+                }
+                l++;
+            }
+
+            if(map.size() <= 2){
+                maxlen = Math.max(maxlen,r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+}
+
+
+
 // import java.util.HashMap;
 
 public class Fruit_Into_Baskets {
