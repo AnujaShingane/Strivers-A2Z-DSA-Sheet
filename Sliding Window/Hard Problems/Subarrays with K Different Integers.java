9f3260d6368340c18b45743004782176697d6c4c
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-19 21:47:51 +0530

    Update Subarrays with K Different Integers.java

--- Sliding Window/Hard Problems/Subarrays with K Different Integers.java
+++ Sliding Window/Hard Problems/Subarrays with K Different Integers.java
@@ -18,6 +18,40 @@
 */
 
 
+My Solution -->
+
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return subarrays(nums, k) - subarrays(nums, k-1);
+    }
+
+    public int subarrays(int[] nums, int k) {
+        int n = nums.length;
+        int l = 0, r = 0;
+        int cnt = 0;
+        if(k < 0) return 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        while(r < n){
+            map.put(nums[r],map.getOrDefault(nums[r],0)+1);
+
+            while(map.size() > k){
+                map.put(nums[l],map.get(nums[l])-1);
+                if(map.get(nums[l]) == 0){
+                    map.remove(nums[l]);
+                }
+                l++;
+            }
+
+            if(map.size() <= k){
+                cnt += r-l+1;
+            }
+            r++;
+        }
+        return cnt;
+    }
+}
+
 // import java.util.HashSet;
 import java.util.HashMap;
 
