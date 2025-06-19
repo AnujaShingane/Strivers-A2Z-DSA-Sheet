480d7544691aa4c5fc0bd483c72b5132c6e61984
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-19 21:44:56 +0530

    Update Binary subarray with sum.java

--- Sliding Window/Medium Problems/Binary subarray with sum.java
+++ Sliding Window/Medium Problems/Binary subarray with sum.java
@@ -20,6 +20,34 @@
 */
 
 
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return numSubarrays(nums,goal) - numSubarrays(nums,goal-1);
+    }
+
+    public int numSubarrays(int[] nums, int goal) {
+        int n = nums.length;
+        int l = 0 , r = 0;
+        int cnt = 0;
+        int sum = 0;
+        if(goal < 0) return 0;
+
+        while(r < n){
+            sum += nums[r];
+            while(sum > goal){
+                sum -= nums[l];
+                l++;
+            }
+            if(sum <= goal){
+                cnt += r-l+1;
+            }
+            r++;
+        }
+        return cnt;
+    }
+}
+
+
 import java.util.HashMap;
 
 public class Binary_Subarrays_With_Sum {
