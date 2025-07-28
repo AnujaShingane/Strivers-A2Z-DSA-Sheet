9ee147859c90fd68b48b53ad93b1a39a20c0e974
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-07-29 01:28:29 +0530

    Merge M sorted Lists.java

--- Heap/Medium Problems/Merge M sorted Lists.java
+++ Heap/Medium Problems/Merge M sorted Lists.java
@@ -9,6 +9,82 @@
  * }
  */
 
+
+main-->
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Push the head of each list into the heap
+        for (ListNode node : lists) {
+            if (node != null) pq.add(node);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            current.next = minNode;
+            current = current.next;
+
+            if (minNode.next != null) {
+                pq.add(minNode.next);  // Push next node of the current list
+            }
+        }
+
+        return dummy.next;
+    }
+}
+
+Time Complexity	- O(N log k)
+Space Complexity - O(k)
+
+
+
+  Second Approch(Your pref)-->
+  /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(ListNode head : lists){
+            ListNode curr = head;
+            while(curr != null){
+                pq.add(curr.val);
+                curr = curr.next;
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while(!pq.isEmpty()){
+            temp.next = new ListNode(pq.poll());
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
+
+
+Time Complexity -	O(N log N)	
+Space Complexity - O(N)	
+
+
+
+
+
+
+
+
 Solution 1: BruteForce Approach:-
   
 /**
