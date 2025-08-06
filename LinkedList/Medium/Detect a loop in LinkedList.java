c0e94d07b45a9abde6f82ed95c059d5972b3d14c
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-07 00:31:31 +0530

    Update Detect a loop in LinkedList.java

--- LinkedList/Medium/Detect a loop in LinkedList.java
+++ LinkedList/Medium/Detect a loop in LinkedList.java
@@ -11,31 +11,9 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
-            return false;
-        }
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode node = head;
-        while(node != null){
-            if(set.contains(node)){
-                return true;
-            }
-            else{
-                set.add(node);
-            }
-            node = node.next;
-        }
-        return false;
-    }
-}
-
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
-            return false;
-        }
-        ListNode fast = head;
         ListNode slow = head;
+        ListNode fast = head;
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
