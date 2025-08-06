14fe701a97ff14324cb51bafa82476921c53f5e0
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-07 00:33:41 +0530

    Update Find the starting point in LL.java

--- LinkedList/Medium/Find the starting point in LL.java
+++ LinkedList/Medium/Find the starting point in LL.java
@@ -11,41 +11,20 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if(head == null || head.next == null){
-            return null;
-        }
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode node = head;
-        while(node != null){
-            if(set.contains(node)){
-                return node;
-            }
-            else{
-                set.add(node);
-            }
-            node = node.next;
-        }
-        return null;
-    }
-}
-
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        if(head == null || head.next == null){
-            return null;
-        }
         ListNode slow = head;
-        ListNode entry = head;
         ListNode fast = head;
+
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
+
             if(slow == fast){
-                while(slow != entry){
-                    entry = entry.next;
+                slow = head;
+                while(slow != fast){
                     slow = slow.next;
+                    fast = fast.next;
                 }
-                return entry;
+                return slow;
             }
         }
         return null;
