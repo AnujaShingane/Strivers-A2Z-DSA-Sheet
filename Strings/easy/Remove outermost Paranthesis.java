e317c9953b8a92370722713d245eb346550276b8
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-10-09 21:48:05 +0530

    Update Remove outermost Paranthesis.java

--- Strings/easy/Remove outermost Paranthesis.java
+++ Strings/easy/Remove outermost Paranthesis.java
@@ -38,48 +38,20 @@
    After removing outer parentheses of each part, this is "" + "" = "".
 */
 
-
-public class Remove_Outermost_Parentheses {
-    public static String removeOuterParentheses(String str) {
-        /* 
-           Time complexity: O(N^2) & Space complexity: O(1). 
-           N^2 due to every time creating a new string object
-
-           String ans = "";
-           int j = 0;
-           for(int i = 0; i < str.length() - 1; i++){
-               if(str.charAt(i) == ')'){
-                   j--;
-               }
-               if(j != 0){
-                   ans += str.charAt(i);
-               }
-               if(str.charAt(i) == '('){
-                   j++;
-               }
-           }
-           return ans;
-        */
- 
-        // Time complexity: O(N) & Space complexity: O(1).
-
-        StringBuilder ans = new StringBuilder();
-        int j = 0;
-        for(int i = 0; i < str.length() - 1; i++){
-            if(str.charAt(i) == ')'){
-                j--;
-            }
-            if(j != 0){
-                ans.append(str.charAt(i));
-            }
-            if(str.charAt(i) == '('){
-                j++;
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int open = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (open > 0) result.append(c);
+                open++;
+            } else {
+                open--;
+                if (open > 0) result.append(c);
             }
         }
-        return ans.toString();
+        return result.toString();
     }
-    public static void main(String[] args) {
-        String str = "(()())(())";
-        System.out.println(removeOuterParentheses(str));
-    }  
 }
