37eb6b7ba78a2225f0b67438cb095b94b9f10f54
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-12 17:39:49 +0530

    Update 1_Partition Set Into 2 Subsets With Min Absolute Sum Diff (DP- 16).java

--- Dynamic Programming/DP on Subsequences/1_Partition Set Into 2 Subsets With Min Absolute Sum Diff (DP- 16).java
+++ Dynamic Programming/DP on Subsequences/1_Partition Set Into 2 Subsets With Min Absolute Sum Diff (DP- 16).java
@@ -20,11 +20,42 @@
     Subset2 = {4}, sum of Subset2 = 4
 */
 
-JUST DO->
 
-totalSum = sum of array
-if(totalSum is odd → false)
-target = totalSum / 2
+S1 + S2 = totalSum
+diff = |S2 - S1|
+S1 is subset sum
+S2 = totalSum - S1
+diff = |totalSum - 2*S1|
+We want minimum diff
+
+
+Tabulation ->
+
+boolean[][] dp = new boolean[n][sum+1];
+
+for(int i=0;i<n;i++) dp[i][0] = true;
+dp[0][arr[0]] = true;
+
+for(int i=1;i<n;i++){
+    for(int t=1;t<=sum;t++){
+        boolean notTake = dp[i-1][t];
+        boolean take = (arr[i]<=t)? dp[i-1][t-arr[i]]:false;
+        dp[i][t] = take || notTake;
+    }
+}
+
+int min = Integer.MAX_VALUE;
+for(int s1=0; s1<=sum/2; s1++){
+    if(dp[n-1][s1]){
+        int s2 = sum - s1;
+        min = Math.min(min, Math.abs(s2 - s1));
+    }
+}
+return min;
+
+
+
+
 
 
 public class Partition_Set_Into_2_Subsets_With_Min_Absolute_Sum_Diff {
