50151d7fdc92508438dd145eba1c114dd8a6cc36
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-12 17:36:47 +0530

    Update 1_Partition Equal Subset Sum (DP- 15).java

--- Dynamic Programming/DP on Subsequences/1_Partition Equal Subset Sum (DP- 15).java
+++ Dynamic Programming/DP on Subsequences/1_Partition Equal Subset Sum (DP- 15).java
@@ -16,117 +16,72 @@
     Explanation: The numsay cannot be partitioned into equal sum subsets.
 */
 
-public class Partition_Equal_Subset_Sum {
-    /*
-       // Recursion
-       public static boolean possible(int idx, int[] nums, int target){
-           if(idx < 0){
-               if(target == 0) return true;
-               return false;
-           }
-   
-           // take
-           boolean take = possible(idx - 1, nums, target - nums[idx]);
-   
-           // not take 
-           boolean notTake = possible(idx - 1, nums, target);
-   
-           return take || notTake;
-       }
-       public static boolean canPartition(int[] nums) {
-           int totalSum = 0, n = nums.length;
-           for(int i = 0; i < n; i++) totalSum += nums[i];
-           if(totalSum % 2 != 0) return false;
-           return possible(n - 1, nums, totalSum/2);
-       }
-    */
+Recursive ->
 
-    /*
-       // Memoization
-       public static boolean possible(int idx, int[] nums, int target, Boolean[][] dp){
-           if(target == 0) return true;
-           if(idx < 0 || target < 0) return false;
-   
-           if(dp[idx][target] != null) return dp[idx][target];
-   
-           // take
-           boolean take = possible(idx - 1, nums, target - nums[idx], dp);
-   
-           // not take 
-           boolean notTake = possible(idx - 1, nums, target, dp);
-   
-           return dp[idx][target] = take || notTake;
-       }
-       public static boolean canPartition(int[] nums) {
-           int totalSum = 0, n = nums.length;
-           for(int i = 0; i < n; i++) totalSum += nums[i];
-           if(totalSum % 2 != 0) return false;
-           Boolean[][] dp = new Boolean[n + 1][(totalSum/2) + 1];
-           return possible(n - 1, nums, totalSum/2, dp);
-       }
-    */
+boolean f(int i, int target, int[] arr){
+    if(target == 0) return true;
+    if(i == 0) return arr[0] == target;
 
-    /*
-       // Tabulation
-       public static boolean canPartition(int[] nums) {
-          int toalSum = 0, n = nums.length;
-          for(int i = 0; i < n; i++) totalSum += nums[i];
-          if(totalSum % 2 != 0) return false;
-          boolean[][] dp = new boolean[n][totalSum + 1];
-          for(int i = 0; i < n; i++){
-              dp[i][0] = true;
-          }
-          if(nums[0]<= sum){
-              dp[0][nums[0]] = true;
-          }
-          
-          for(int idx = 1; idx < n; idx++){
-              for(int target = 1; target <= totalSum/2; target++){
-                  // take
-                  boolean take = false;
-                  if(nums[idx] <= target) take = dp[idx - 1][target - nums[idx]];
-                      
-                  // not-take
-                  boolean notTake = dp[idx - 1][target];
-                  
-                  dp[idx][target] = take || notTake;
-              }
-          }
-          return dp[n - 1][totalSum/2];
-       }
-    */
-    
-    // Space Optimization
-    public static boolean canPartition(int[] nums) {
-        int totalSum = 0, n = nums.length;
-        for(int i = 0; i < n; i++) totalSum += nums[i];
-        if(totalSum % 2 != 0) return false;
-        boolean[] prev = new boolean[totalSum + 1];
-        boolean[] curr = new boolean[totalSum + 1];
-        
-        prev[0] = curr[0] = true;
-        
-        if(nums[0]<= totalSum){
-            prev[nums[0]] = true;
-        }
-        
-        for(int idx = 1; idx < n; idx++){
-            for(int target = 1; target <= totalSum/2; target++){
-                // take
-                boolean take = false;
-                if(nums[idx] <= target) take = prev[target - nums[idx]];
-                    
-                // not-take
-                boolean notTake = prev[target];
-                
-                curr[target] = take || notTake;
-            }
-            prev = curr.clone();
-        }
-        return prev[totalSum/2];
-    }
-    public static void main(String[] args) {
-        int[] nums = {1,5,11,5};
-        System.out.println(canPartition(nums));
+    boolean notTake = f(i-1, target, arr);
+    boolean take = false;
+    if(arr[i] <= target)
+        take = f(i-1, target - arr[i], arr);
+
+    return take || notTake;
+}
+
+
+Memoization -> 
+
+    Boolean[][] dp;
+
+boolean f(int i, int target, int[] arr){
+    if(target == 0) return true;
+    if(i == 0) return arr[0] == target;
+
+    if(dp[i][target] != null) return dp[i][target];
+
+    boolean notTake = f(i-1, target, arr);
+    boolean take = false;
+    if(arr[i] <= target)
+        take = f(i-1, target - arr[i], arr);
+
+    return dp[i][target] = take || notTake;
+}
+
+
+Tabulation ->
+
+boolean[][] dp = new boolean[n][k+1];
+
+for(int i=0;i<n;i++) dp[i][0] = true;
+if(arr[0] <= k) dp[0][arr[0]] = true;
+
+for(int i=1;i<n;i++){
+    for(int t=1;t<=k;t++){
+        boolean notTake = dp[i-1][t];
+        boolean take = (arr[i]<=t)? dp[i-1][t-arr[i]] : false;
+        dp[i][t] = take || notTake;
     }
 }
+return dp[n-1][k];
+
+    
+Space Optimization ->
+
+boolean[] prev = new boolean[k+1];
+prev[0] = true;
+if(arr[0] <= k) prev[arr[0]] = true;
+
+for(int i=1;i<n;i++){
+    boolean[] cur = new boolean[k+1];
+    cur[0] = true;
+    for(int t=1;t<=k;t++){
+        boolean notTake = prev[t];
+        boolean take = (arr[i]<=t)? prev[t-arr[i]] : false;
+        cur[t] = take || notTake;
+    }
+    prev = cur;
+}
+return prev[k];
+
