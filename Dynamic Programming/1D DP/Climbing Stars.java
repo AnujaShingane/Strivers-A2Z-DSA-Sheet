6ff95ea0906b20227ee9fe9ca818145d67a8f87b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-07-14 10:57:05 +0530

    Improve problem statement formatting in Climbing Stars
    Reformatted problem statement for clarity and structure.

--- Dynamic Programming/1D DP/Climbing Stars.java
+++ Dynamic Programming/1D DP/Climbing Stars.java
@@ -4,9 +4,11 @@
     How to write 1-D Recurrence relation / Climbing Stairs
     In this article, we will learn to write 1-D Recurrence relations using the problem “Climbing Stairs”
     
-    Problem Statement: Given a number of stairs. Starting from the 0th stair we need to climb to the “Nth” stair. At a time we can climb either one or two steps. We need to return the total number of distinct ways to reach from 0th to Nth stair.
-  
-    There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top (order does matter).
+    Problem Statement: Given a number of stairs. Starting from the 0th stair we need to climb to the “Nth” stair. 
+    At a time we can climb either one or two steps. We need to return the total number of distinct ways to reach from 0th to Nth stair.
+    
+    There are n stairs, a person standing at the bottom wants to reach the top. 
+    The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top (order does matter).
   
     Example 1:
     
