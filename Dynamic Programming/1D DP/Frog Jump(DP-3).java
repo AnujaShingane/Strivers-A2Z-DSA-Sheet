dd46cf49483d5523c75ad7be2afa3e5e5a06b48b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-07-14 11:15:45 +0530

    Update Frog Jump(DP-3).java

--- Dynamic Programming/1D DP/Frog Jump(DP-3).java
+++ Dynamic Programming/1D DP/Frog Jump(DP-3).java
@@ -2,7 +2,11 @@
     Q. Dynamic Programming : Frog Jump (DP 3) 
     Practice : https://practice.geeksforgeeks.org/problems/geek-jump/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=geek-jump
     Geek Jump
-    Geek wants to climb from the 0th stair to the (n-1)th stair. At a time the Geek can climb either one or two steps. A height[N] array is also given. Whenever the geek jumps from stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j]), where abs() means the absolute difference. return the minimum energy that can be used by the Geek to jump from stair 0 to stair N-1.
+    Geek wants to climb from the 0th stair to the (n-1)th stair. 
+    At a time the Geek can climb either one or two steps. 
+    A height[N] array is also given. Whenever the geek jumps from stair i to stair j, 
+    the energy consumed in the jump is abs(height[i]- height[j]), where abs() means the absolute difference. 
+    return the minimum energy that can be used by the Geek to jump from stair 0 to stair N-1.
     
     Example:
     Input:
