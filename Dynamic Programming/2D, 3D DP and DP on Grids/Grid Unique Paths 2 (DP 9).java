b4f0fd61cf352c64f387850b832fe726b50d8b05
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-07-14 12:23:41 +0530

    Update Grid Unique Paths 2 (DP 9).java

--- Dynamic Programming/2D, 3D DP and DP on Grids/Grid Unique Paths 2 (DP 9).java
+++ Dynamic Programming/2D, 3D DP and DP on Grids/Grid Unique Paths 2 (DP 9).java
@@ -1,9 +1,13 @@
 /*
     Q. Grid Unique Paths 2 (DP 9)
     Practice : https://leetcode.com/problems/unique-paths-ii/description/
-    You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
-
-    An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+    You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). 
+    The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+    
+    The robot can only move either down or right at any point in time.
+    
+    An obstacle and space are marked as 1 or 0 respectively in grid. 
+    A path that the robot takes cannot include any square that is an obstacle.
     
     Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
     
